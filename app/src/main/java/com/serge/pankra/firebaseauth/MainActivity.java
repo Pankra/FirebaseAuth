abf96ffd590be317a9b4378f09e8a063f867ac25
pankratov <sergbk17@mail.ru>
2018-04-20 18:31:35 +0500

    firebase settings example

--- app/src/main/java/com/serge/pankra/firebaseauth/MainActivity.java
+++ app/src/main/java/com/serge/pankra/firebaseauth/MainActivity.java
@@ -1,11 +1,14 @@
 package com.serge.pankra.firebaseauth;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -16,26 +19,46 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String BACKGROUND_COLOR = "background_color";
+    RelativeLayout root;
     EditText phone;
     Button authBtn;
     EditText confirmCode;
     Button confirmCodeBtn;
     private String verificationId;
     private FirebaseAuth auth;
+    private FirebaseRemoteConfig remoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        root = findViewById(R.id.root);
         phone = findViewById(R.id.phone);
         authBtn = findViewById(R.id.auth);
         confirmCode = findViewById(R.id.confirmCode);
         confirmCodeBtn = findViewById(R.id.codeConfirmBtn);
+
+        remoteConfig = FirebaseRemoteConfig.getInstance();
+        remoteConfig.setDefaults(R.xml.remote_config_defaults);
+        // Create a Remote Config Setting to enable developer mode, which you can use to increase
+        // the number of fetches available per hour during development. See Best Practices in the
+        // README for more information.
+        // [START enable_dev_mode]
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        remoteConfig.setConfigSettings(configSettings);
+        // [END enable_dev_mode]
+
+        fetchRemoteConfig();
 
         auth = FirebaseAuth.getInstance();
         auth.useAppLanguage();
@@ -84,6 +107,43 @@ public class MainActivity extends AppCompatActivity {
                 singInUser(credential);
             }
         });
+    }
+
+    private void fetchRemoteConfig() {
+        String bgColor = remoteConfig.getString(BACKGROUND_COLOR);
+        root.setBackgroundColor(Color.parseColor(bgColor));
+
+        long cacheExpiration = 3600; // 1 hour in seconds.
+        // If your app is using developer mode, cacheExpiration is set to 0, so each fetch will
+        // retrieve values from the service.
+        if (remoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpiration = 0;
+        }
+
+        // [START fetch_config_with_callback]
+        // cacheExpirationSeconds is set to cacheExpiration here, indicating the next fetch request
+        // will use fetch data from the Remote Config service, rather than cached parameter values,
+        // if cached parameter values are more than cacheExpiration seconds old.
+        // See Best Practices in the README for more information.
+        remoteConfig.fetch(cacheExpiration)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Fetch Succeeded",
+                                    Toast.LENGTH_SHORT).show();
+
+                            // After config data is successfully fetched, it must be activated before newly fetched
+                            // values are returned.
+                            remoteConfig.activateFetched();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Fetch Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        root.setBackgroundColor(Color.parseColor(remoteConfig.getString(BACKGROUND_COLOR)));
+                    }
+                });
+        // [END fetch_config_with_callback]
     }
 
     private void singInUser(PhoneAuthCredential phoneAuthCredential) {
